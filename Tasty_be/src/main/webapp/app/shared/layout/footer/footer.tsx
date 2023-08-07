7957b42e65e30058181d9a67df0b4842647f5d29
tiennd <tiennd@hivetech.vn>
2023-08-07 12:01:41 +0700

    Create entity jdl

--- Tasty_be/src/main/webapp/app/shared/layout/footer/footer.tsx
+++ Tasty_be/src/main/webapp/app/shared/layout/footer/footer.tsx
@@ -1,16 +1,34 @@
 import './footer.scss';
+import {
+  FaPinterestSquare,
+  FaInstagram,
+  FaTwitter,
+  FaFacebookSquare,
+} from 'react-icons/fa'
 
 import React from 'react';
 
 import { Col, Row } from 'reactstrap';
 
 const Footer = () => (
-  <div className="footer page-content">
-    <Row>
-      <Col md="12">
-        <p>This is your footer</p>
-      </Col>
-    </Row>
+  <div className="footer-container">
+    <div className="footer-heading-container">
+      <img
+        src="https://res.cloudinary.com/nsp/image/upload/v1635840304/tastyKitchens/logowhite_t8wfhc.png"
+        alt="website-footer-logo"
+        className="website-footer-logo"
+      />
+      <h1 className="footer-heading">Tasty Kitchens</h1>
+    </div>
+    <p className="footer-para">
+      The only thing we are serious about is food. Contact us on
+    </p>
+    <div className="social-container">
+      <FaPinterestSquare className="social-icon" />
+      <FaInstagram className="social-icon" />
+      <FaTwitter className="social-icon" />
+      <FaFacebookSquare className="social-icon" />
+    </div>
   </div>
 );
 
