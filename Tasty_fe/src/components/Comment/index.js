4ce4b27a55b581c9f32bf83c92bf33d4cc6bd8c7
Nguyen Dinh Tien <zeezxt@gmail.com>
2023-09-14 18:50:37 +0700

    comment

--- Tasty_fe/src/components/Comment/index.js
+++ Tasty_fe/src/components/Comment/index.js
@@ -1,49 +1,97 @@
 import {Component} from "react";
 import './index.css'
+import Cookies from 'js-cookie'
 
 class Comment extends Component {
     state = {
-        comments: [
-            {id: 1, username: 'abc', comment: '123456789', time: '12 phút trước'},
-            {id: 2, username: 'efg', comment: '10111213141516171819', time: '2 giờ trước'},
-            {id: 3, username: 'hij', comment: '20212223242526272829', time: '5 giờ trước'},
-            {id: 4, username: 'klm', comment: '30313233343536373839', time: '8 giờ trước'},
-            {id: 5, username: 'nop', comment: '40414243444546474849', time: '20 giờ trước'},
-            {id: 6, username: 'qrs', comment: '50515253545556575859', time: '2 ngày trước'},
-        ],
-        subComments: [
-            {id: 1, username: 'abc', comment: '123456789', superComment: 1, time: 'Vừa xong'},
-            {id: 2, username: 'efg', comment: '10111213141516171819', superComment: 1, time: '30 phút trước'},
-            {id: 3, username: 'hij', comment: '20212223242526272829', superComment: 1, time: '12 giờ trước'},
-            {id: 4, username: 'klm', comment: '30313233343536373839', superComment: 2, time: '1 ngày trước'},
-            {id: 5, username: 'nop', comment: '40414243444546474849', superComment: 2, time: '1 tháng trước'},
-            {id: 6, username: 'qrs', comment: '50515253545556575859', superComment: 3, time: '1 năm trước'},
-        ],
+        comments: [],
+        subComments: [],
         addSub: NaN,
         addComment: '',
         addSubComment: '',
+        avatar: '',
+    }
+
+    componentDidMount() {
+        this.loadComment()
+    }
+
+    loadAvatar = async () => {
+        console.log(1)
+        const username = Cookies.get('username')
+        console.log(username)
+        const apiUrl = `http://localhost:8080/api/customer/${username}/avatar`
+        const options = {
+            method: 'GET',
+        }
+        const response = await fetch(apiUrl, options)
+        if (response.ok) {
+            console.log(response)
+            const fetchedData = await response.json()
+            this.setState({avatar: fetchedData.url})
+        } else {
+            this.setState({avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSheI9UkWllIpSNbs2UdE18KLLswgDON9qzXg&usqp=CAU'})
+            console.log("Load avatar error.")
+        }
+    }
+
+    loadComment = async () => {
+        const postsId = this.props.postsId
+        const apiUrl = `http://localhost:8080/api/customer/posts/${postsId}/comment`
+        const options = {
+            method: 'GET',
+        }
+        const response = await fetch(apiUrl, options)
+        if (response.ok) {
+            const fetchedData = await response.json()
+            const subComments = fetchedData.subComments
+            const comments = fetchedData.comments
+            this.setState({comments, subComments})
+            this.loadAvatar()
+        } else {
+            alert("Gặp lỗi trong khi tải bình luận.")
+        }
     }
 
     addComment = () => {
+        const postsId = this.props.postsId
         const {addComment, comments} = this.state
-        const maxId = Math.max(...comments.map(c => c.id))
-        comments.unshift({id: maxId + 1, comment: addComment, time: 'Vừa xong', username: 'xyz'})
-        this.setState({comments})
         document.getElementById('comment-input').innerText = ''
+        const data = {
+            subComment: false,
+            comment: addComment,
+            postsId: parseInt(postsId)
+        }
+        this.saveComment(data)
     }
 
     addSubComment = id => {
-        const {addSubComment, subComments} = this.state
-        const maxId = Math.max(...subComments.map(c => c.id))
-        subComments.unshift({
-            id: maxId + 1,
-            comment: addSubComment,
-            time: 'Vừa xong',
-            username: 'xyz',
-            superComment: id
-        })
-        this.setState({subComments})
+        const postsId = this.props.postsId
+        const {addSubComment} = this.state
         document.getElementById(id).innerText = ''
+        const data = {
+            subComment: true,
+            comment: addSubComment,
+            postsId: postsId,
+            supperCommentId: parseInt(id)
+        }
+        this.saveComment(data)
+    }
+
+    saveComment = async (data) => {
+        const jwtToken = Cookies.get('jwt_token')
+        const apiUrl = 'http://localhost:8080/api/customer/comment'
+        const options = {
+            headers: {
+                Authorization: `Bearer ${jwtToken}`,
+                Accept: 'application/json',
+                'Content-type': 'application/json',
+            },
+            body: JSON.stringify(data),
+            method: 'POST',
+        }
+        await fetch(apiUrl, options)
+        this.loadComment()
     }
 
     onKeyDownComment = (e) => {
@@ -63,86 +111,105 @@ class Comment extends Component {
         this.setState({addSubComment: e.currentTarget.innerText})
     }
 
-    render() {
-        const {comments, subComments, addSub} = this.state
+    deleteComment = async (id) => {
+        const apiUrl = `http://localhost:8080/api/customer/comment/${id}`
+        const options = {
+            method: 'DELETE',
+        }
+        await fetch(apiUrl, options)
+        this.loadComment()
+    }
 
-        return (
-            <div className="comment-container">
-                <div className="input-comment">
-                    <p className="comment-title">Bình luận:</p>
-                    <div className="comment-action">
+    render()
+        {
+            const {comments, subComments, addSub} = this.state
+            const username = Cookies.get('username')
+
+            return (
+                <div className="comment-container">
+                    <div className="input-comment">
+                        <p className="comment-title">Bình luận:</p>
+                        <div className="comment-action">
                         <span id="comment-input" role="textbox" contentEditable className="comment-input"
                               onKeyDown={this.onKeyDownComment}/>
-                        <button className='primaryContained float-right' type="submit" onClick={this.addComment}>
-                            <span className="material-symbols-rounded">send</span>
-                        </button>
+                            <button className='primaryContained float-right' type="submit" onClick={this.addComment}>
+                                <span className="material-symbols-rounded">send</span>
+                            </button>
+                        </div>
                     </div>
-                </div>
-                <section className="comment-module">
-                    <ul className="list-comments">
-                        {comments.map(comment => (
-                            <li>
-                                <div className="comment">
-                                    <div className="comment-img">
-                                        <img src="https://rvs-comment-module.vercel.app/Assets/User Avatar.png" alt=""/>
-                                    </div>
-                                    <div className="comment-content">
-                                        <div className="comment-details">
-                                            <h4 className="comment-name">{comment.username}</h4>
-                                            <span className="comment-log">{comment.time}</span>
+                    <section className="comment-module">
+                        <ul className="list-comments">
+                            {comments.map(comment => (
+                                <li>
+                                    <div className="comment">
+                                        <div className="comment-img">
+                                            <img src={comment.avatarUrl} alt=""/>
                                         </div>
-                                        <div className="comment-desc">
-                                            <p>{comment.comment}</p>
-                                        </div>
-                                        <div className="comment-data">
-                                            <div className="comment-reply">
-                                                <a className="reply-button"
-                                                   onClick={() => this.setState({addSub: comment.id})}>Trả lời
-                                                    <span className="material-symbols-rounded reply-icon">reply</span>
-                                                </a>
+                                        <div className="comment-content">
+                                            <div className="comment-details">
+                                                <h4 className="comment-name">{comment.username}</h4>
+                                                <span className="comment-log">{comment.time}</span>
+                                                {comment.username === username && (
+                                                    <span className="material-symbols-rounded delete-button"
+                                                    onClick={() => this.deleteComment(comment.id)}>delete</span>
+                                                )}
+                                            </div>
+                                            <div className="comment-desc">
+                                                <p>{comment.comment}</p>
+                                            </div>
+                                            <div className="comment-data">
+                                                <div className="comment-reply">
+                                                    <a className="reply-button"
+                                                       onClick={() => this.setState({addSub: comment.id})}>Trả lời
+                                                        <span
+                                                            className="material-symbols-rounded reply-icon">reply</span>
+                                                    </a>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
-                                </div>
-                                <ul className="list-comments">
-                                    <div className={`input-sub-comment ${addSub === comment.id ? '' : 'disable'}`}>
+                                    <ul className="list-comments">
+                                        <div className={`input-sub-comment ${addSub === comment.id ? '' : 'disable'}`}>
                                         <span role="textbox" contentEditable id={comment.id} className="comment-input"
                                               onKeyDown={this.onKeyDownSubComment}/>
-                                        <button className='primaryContained float-right' type="submit"
-                                                onClick={() => this.addSubComment(comment.id)}>
-                                            <span className="material-symbols-rounded">send</span>
-                                        </button>
-                                    </div>
-                                    {subComments.filter(sc => {
-                                        return sc.superComment === comment.id
-                                    }).map(subComment => (
-                                        <li>
-                                            <div className="comment">
-                                                <div className="comment-img">
-                                                    <img
-                                                        src="https://rvs-comment-module.vercel.app/Assets/User Avatar-1.png"
-                                                        alt=""/>
-                                                </div>
-                                                <div className="comment-content">
-                                                    <div className="comment-details">
-                                                        <h4 className="comment-name">{subComment.username}</h4>
-                                                        <span className="comment-log">{subComment.time}</span>
+                                            <button className='primaryContained float-right' type="submit"
+                                                    onClick={() => this.addSubComment(comment.id)}>
+                                                <span className="material-symbols-rounded">send</span>
+                                            </button>
+                                        </div>
+                                        {subComments.filter(sc => {
+                                            return sc.superComment === comment.id
+                                        }).map(subComment => (
+                                            <li>
+                                                <div className="comment">
+                                                    <div className="comment-img">
+                                                        <img
+                                                            src={subComment.avatarUrl}
+                                                            alt=""/>
                                                     </div>
-                                                    <div className="comment-desc">
-                                                        <p>{subComment.comment}</p>
+                                                    <div className="comment-content">
+                                                        <div className="comment-details">
+                                                            <h4 className="comment-name">{subComment.username}</h4>
+                                                            <span className="comment-log">{subComment.time}</span>
+                                                            {subComment.username === username && (
+                                                                <span className="material-symbols-rounded delete-button">delete</span>
+                                                            )}
+                                                        </div>
+                                                        <div className="comment-desc">
+                                                            <p>{subComment.comment}</p>
+                                                        </div>
                                                     </div>
                                                 </div>
-                                            </div>
-                                        </li>
-                                    ))}
-                                </ul>
-                            </li>
-                        ))}
-                    </ul>
-                </section>
-            </div>
-        )
+                                            </li>
+                                        ))}
+                                    </ul>
+                                </li>
+                            ))}
+                        </ul>
+                    </section>
+                </div>
+            )
+        }
     }
-}
 
 export default Comment
